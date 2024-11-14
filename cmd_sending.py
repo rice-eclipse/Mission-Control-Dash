f441103ed5ed8f17e951582bc7a350296985ba4e
vizdiz <vismayravikumar@gmail.com>
2024-11-13 18:47:24 -0600

    Added a method for receiving data from Quonkboard

--- cmd_sending.py
+++ cmd_sending.py
@@ -1,6 +1,7 @@
 import asyncio
 import asyncpio
 from data_handling import DataHandler
+import json
 
 
 #TODO: Pin definitions
@@ -11,19 +12,22 @@ VALVE2 = 1
 VALVE3 = 3
 VALVE4 = 4
 
+VALVE_THRESHOLD = 0.5
 ON = 1
+OFF = 0
 
 class CmdSender:
 
-    def __init__(self,drivers: dict, driver_commands: dict, data_handler:DataHandler)-> None:
-        self.drivers = drivers,
-        self.commands = driver_commands,
-        self.data_handler = data_handler
+    def __init__(self,drivers: dict, driver_commands: dict, client = None)-> None:
+        self.drivers = drivers
+        self.commands = driver_commands
+        self.client = client
 
     #Loops through each switch and checks if its state has been changed
     #sends the command to actuate/deactuate driver
     async def driver_control(self):
-        await self.update_driver_states()
+        await self.received_states()
+        self.update_drivers()
         for driver in self.drivers:
             if (driver == "Ignition"):
                 driver["key_state"] = await self.read_pin(driver["key_pin"])
@@ -31,7 +35,7 @@ class CmdSender:
 
                 #sends ignition command if both of the pins are high
                 if (driver["key_state"] and driver["btn_state"]):
-                    self.commands["Ignition"]["ignite"] = 1;
+                    self.commands["Ignition"]["ignite"] = 1
                     
             else:
                 driver["switch_state"] = await self.read_pin(driver["switch_pin"]);
@@ -42,7 +46,33 @@ class CmdSender:
                         self.commands[driver]["deactuate"] = 1
         #send the message object
         await self.send_command()
-        self.reset_commands()
+
     async def read_pin(pin: int, driver: str):
         driver["switch_state"] = await asyncpio.read_pin(pin)
         
+
+    async def recieve_states(self):
+        if self.client == None:
+            await asyncio.sleep(0.1)
+
+        message = await self.client.recv()
+        states = json.load(message)
+        
+        for key in ["Engine_Vent", "Ground_Purge", "Isolation"]:
+            current = states[key]["valve_current"]
+            self.drivers[key]["valve_current"] = current
+            
+            if current > VALVE_THRESHOLD:
+                self.drivers[key]['led_pin'] = ON
+            else:
+                self.drivers[key]['led_pin'] = OFF
+            
+            self.write_pin(self.drivers[key]['current_display_pin'], current)
+
+    # TODO: implement wrapper for read_pin
+    def read_pin(pin: int, driver: str):
+        pass
+    
+    # TODO: implement write_pin
+    def write_pin(pin: int, val: int):
+        pass
