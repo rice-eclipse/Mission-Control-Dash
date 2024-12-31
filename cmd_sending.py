362043ce1eb3f96b8bf6ac0aea98bb6effa9aed4
deepsn458 <deepaknarayan458@gmail.com>
2024-12-31 08:41:24 -0600

    added GPIO and i2c interfaces

--- cmd_sending.py
+++ cmd_sending.py
@@ -1,20 +1,14 @@
 import asyncio
-import asyncpio
+from typing import Self
 from main import drivers
+from gpiozero import LED, Button
+import busio
+import board
+from adafruit_ht16k33 import segments
 import json
 
-
-#TODO: Pin definitions
-IGNITION_BUTTON = 10
-IGNITION_KEY = 20
-VALVE1 = 0
-VALVE2 = 1
-VALVE3 = 3
-VALVE4 = 4
-
-VALVE_THRESHOLD = 0.5
-ON = 1
-OFF = 0
+#minimum current needed to open a valve
+VALVE_THRESHOLD = 1
 
 class CmdSender:
 
@@ -34,8 +28,8 @@ class CmdSender:
         for driver in self.drivers:
             #updates the state of the ignition driver
             if (driver == "Ignition"):
-                driver["key_state"] = await self.read_pin(driver["key_pin"])
-                driver["btn_state"] = await self.read_pin(driver["btn_pin"])
+                driver["key_state"] = self.read_pin(driver["key_pin"])
+                driver["btn_state"] = self.read_pin(driver["btn_pin"])
 
                 #sends ignition command if both of the pins are high
                 if (driver["key_state"] and driver["btn_state"]):
@@ -43,8 +37,10 @@ class CmdSender:
 
             #updates the state of the other drivers      
             else:
-                driver["switch_state"] = await self.read_pin(driver["switch_pin"]);
-                if (driver["switch_state"] != driver["led_state"]):
+                driver["switch_state"] = self.read_pin(driver["switch_pin"])
+
+                #sends a command if the valve state and the switch state are different
+                if (driver["switch_state"] != driver["valve_state"]):
                     if (driver["switch_state"] == 1):
                         self.commands[driver]["actuate"] = 1
                     else:
@@ -53,11 +49,11 @@ class CmdSender:
         await self.send_command()
         self.reset_command_states()
 
-    async def read_pin(pin: int, driver: str) -> None:
-        driver["switch_state"] = await asyncpio.read_pin(pin)
+    def read_pin(pin: int) -> int:
+       return int(Button(pin).is_pressed)
         
 
-    async def recieve_states(self):
+    async def recieve_states(self) -> None:
         while self.client == None:
             await asyncio.sleep(0.1)
 
@@ -67,27 +63,36 @@ class CmdSender:
         for key in drivers.key():
             current = states[key]["valve_current"]
             self.drivers[key]["valve_current"] = current
+            self.display_current(self.drivers[key]["current_display_obj"], current)
             
+            #turns on the led if 
             if current > VALVE_THRESHOLD:
-                self.drivers[key]['led_pin'] = ON
+                self.drivers[key]['led_pin'] = 0
+                self.write_led(self.drivers[key]['led_pin'], 0)
             else:
-                self.drivers[key]['led_pin'] = OFF
-            
-            self.write_pin(self.drivers[key]['current_display_pin'], current)
+                self.drivers[key]['led_pin'] = 1
+                self.write_led(self.drivers[key]['led_pin'], 1)
 
-    async def send_command(self):
+    async def send_command(self) -> None:
         while self.client == None:
             await asyncio.sleep(0.1)
         self.client.send(json.dumps(self.driver_commands))
     
-    async def add_client(self, client):
+    async def add_client(self, client) -> None:
         self.client = client
     
-    # TODO: implement write_pin
-    def write_pin(pin: int, val: int):
-        pass
+    def write_led(self,pin: int, val: int) -> None:
+        if (int == 1):
+            LED(pin).on()
+        else:
+            LED(pin).off()
 
-    def reset_command_states(self):
+    def reset_command_states(self) -> None:
         for driver in self.commands:
             for command in self.commands[driver]:
                 self.commands[driver][command] = 0
+    
+    def display_current(self, display_obj, current) -> None:
+        display_obj.fill(0)
+        display_obj.print(current)
+        
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import asyncio
+from gpiozero import Button, LED
 from multiprocessing.resource_sharer import stop
 from websockets.asyncio.server import ServerConnection, serve
 import json
 from cmd_sending import CmdSender
 from data_handling import DataHandler
-import pigpio
+import busio
+import board
+from adafruit_ht16k33 import segments
 import time
 
 #TODO: Pinout definitions (move to config too)
@@ -13,34 +16,37 @@ import time
 drivers =  {
     "Oxidizer_Fill": {
         "valve_current": 0, # Data from quonkboard
-        "led_state": 0, #Use Data from quonkboard to update
+        "valve_state": 0, #Use Data from quonkboard to update
         "switch_state": 0, # read switch on MC dashboard
-        "current_display_pin": 3, #Use Data from quonkboard to update
+        "current_display_address": 0x70, #Use Data from quonkboard to update
+        "current_display_obj" : None,
         "led_pin": 22, #output pin
         "switch_pin": 5
 
     },
     "Ground_Vent":{
         "valve_current": 0,# Data from quonkboard
-        "led_state": 0, #Use Data from quonkboard to update
+        "valve_state": 0, #Use Data from quonkboard to update
         "switch_state": 0, # read switch on MC dashboard
-        "current_display_pin": 3, #Use Data from quonkboard to update
+        "current_display_address": 0x71, #Use Data from quonkboard to update
         "led_pin": 23,
         "switch_pin": 6
     },
     "OPS_Pneumatic":{
         "valve_current": 0,# Data from quonkboard
-        "led_state": 0, #Use Data from quonkboard to update
+        "valve_state": 0, #Use Data from quonkboard to update
         "switch_state": 0, # read switch on MC dashboard
-        "current_display_pin": 3, #Use Data from quonkboard to update
+        "current_display_address": 0x72, #Use Data from quonkboard to update
+        "current_display_obj" : None,
         "led_pin": 24,
         "switch_pin": 16
     },
     "Engine_Vent":{
         "valve_current": 0,# Data from quonkboard
-        "led_state": 0, #Use Data from quonkboard to update
+        "valve_state": 0, #Use Data from quonkboard to update
         "switch_state": 0, # read switch on MC dashboard
-        "current_display_pin": 3, #Use Data from quonkboard to update
+        "current_display_address": 0x73, #Use Data from quonkboard to update
+        "current_display_obj" : None,
         "led_pin": 25,
         "switch_pin": 17
     },
@@ -69,28 +75,33 @@ driver_commands = {
         "ignite": 0
     }
 }
-pi = pigpio.pi()
 
-#Ensures all the valve switches are off to avoid accidental actuation on startup 
+
+
+# Ensures all the valve switches are off to avoid accidental actuation on startup 
+# creates all the i2c objects for the 7 segment displays
 def initiate() -> None:
+    i2c = busio.I2C(board.SCL, board.SDA)
     for driver in drivers:
+        drivers[driver]["current_display_obj"] = segments.Seg7x4(i2c, address=
+                                                                 drivers[driver]["current_display_address"])
+        drivers[driver]["current_display_obj"].fill(0)
         ensure_state_off(driver)
-
+        
 #keeps looping until the operator turns the switch off
 def ensure_state_off(driver: dict) -> None:
     #checks if the driver is a valve or ignition
     driver_pin =  "key_pin" if driver == "Ignition" else "switch_pin"
-    while (read_pin(driver[driver_pin], driver)==1):
+    while (read_pin(driver[driver_pin])==1):
         #TODO: Some kind of alert mechanism
         time.sleep(0.1)
-    driver["key_state"] = read_pin(driver["key_pin"], driver)
+        
+    #should be 0
+    driver["key_state"] = read_pin(driver[driver_pin])
 
-def read_pin(pin: int, driver: str) -> int:
-    return pi.read_pin(pin)
 
 async def main():
     initiate()
-
     async with CmdSender(drivers, driver_commands, pi) as cmd_sender:
         async def handler(websocket):
             await cmd_sender.send_command(websocket)
@@ -101,7 +112,10 @@ async def main():
     async with serve(handler, "",5000):
         await stop_signal
 
-
+#wrapping the reading of the pin in a separate function in case we migrate to a different
+#python gpio library
+def read_pin(pin: int) -> int:
+    return int(Button(pin).is_pressed)
 
 
 if __name__== "__main__":
